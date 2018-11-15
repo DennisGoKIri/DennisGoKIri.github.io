95737fbd05d0550caf56ae5cf94487fed3c95c79
DennisGoKIri <wewutong@icloud.com>
2018-11-15 10:49:57 +0800

    Site updated: 2018-11-15 10:49:57

---
+++ dist/music.js
@@ -0,0 +1,9 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [{
+        name: 'MO14',
+        artist: '梶浦由記',
+        url: 'http://music.163.com/song/media/outer/url?id=591839.mp3',
+        cover: 'http://p1.music.126.net/8evxqelgme7kUAph_DxyQA==/3191882255508479.jpg?param=130y130'
+    }]
+});
